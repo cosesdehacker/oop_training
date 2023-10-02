60c1d0848ae543a3178af5104cb0d581c7fc8914
cristina.mateos <cristina.mateos@scrm.eu>
2023-10-02 15:58:33 +0200

    removed prints

--- emanuele/advent_coding/day_1/main.py
+++ emanuele/advent_coding/day_1/main.py
@@ -3,8 +3,6 @@ def read_to_list(file):
     input_data = open(file, "r").read()
     input_data_list = input_data.split("\n")
 
-    input_data_list = ["2","2","","0","98","","3"]
-
     i = 0
     output_lists_list = []
     output_list = []
@@ -20,43 +18,26 @@ def read_to_list(file):
             output_list = []
         i = i+1
     structured_input = output_lists_list
-    print("structured_input:")
-    print(structured_input)
     return structured_input
 
 
 def sum_values_in_list(file):
     structured_input = read_to_list(file)
     total_calories_by_elf = []
-    print(structured_input)
     for el in structured_input:
-        print("el:")
-        print(el)
         i = 0
         total_calories = 0
         while i < len(el):
-            print("total_calories1:")
-            print(total_calories)
             total_calories = total_calories + int(el[i])
-            print("iterator:")
-            print(i)
-            print("total_calories2:")
-            print(total_calories)
             i = i+1
         total_calories_by_elf.append(total_calories)
-    print("total calories by elf:")
-    print(total_calories_by_elf)
     return total_calories_by_elf
 
 
 def get_most_caloric_elf(file):
     total_calories_by_elf = sum_values_in_list(file)
     max_calories = max(total_calories_by_elf)
-    print("max_calories:")
-    print(max_calories)
     most_caloric_elf = total_calories_by_elf.index(max_calories)+1
-    print("most_caloric_elf, max_calories")
-    print(most_caloric_elf, max_calories)
     return most_caloric_elf, max_calories
 
 
