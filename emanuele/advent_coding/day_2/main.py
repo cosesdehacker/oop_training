2228008f481a984fbfe2f777b375573293fde36d
cristina.mateos <cristina.mateos@scrm.eu>
2023-10-02 23:48:14 +0200

    day 2 exercise

---
+++ emanuele/advent_coding/day_2/main.py
@@ -0,0 +1,15 @@
+
+
+
+if __name__ == '__main__':
+    file = "input.txt"
+    #part 1
+    print("PART 1\n")
+
+    print(f"Total score would be ")
+
+    #part 2
+    print("PART 2\n")
+
+    print(f"")
+
