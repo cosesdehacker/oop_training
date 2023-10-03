31822af2313b6b152f1422d0932853d18258a757
cristina.mateos <cristina.mateos@scrm.eu>
2023-10-03 21:26:32 +0200

    day 2 second exercise

--- emanuele/advent_coding/day_2/main.py
+++ emanuele/advent_coding/day_2/main.py
@@ -1,11 +1,8 @@
 '''
-first column (oponent): A for Rock, B for Paper, and C for Scissors
-second column (me): X for Rock, Y for Paper, and Z for Scissors
-
-total score = the sum of your scores for each round
-score for a single round = sum of:
-- the score for the shape you selected: 1 for Rock, 2 for Paper, and 3 for Scissors
-- the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won
+the second column says how the round needs to end:
+- X means you need to lose
+- Y means you need to end the round in a draw
+- Z means you need to win
 '''
 
 
@@ -27,37 +24,54 @@ def read_to_list(file):
 def final_result(file):
     shape_score_dict = {'X': 1, 'Y': 2, 'Z': 3}
     win_options = {('C', 'X'), ('A', 'Y'), ('B', 'Z')}
+    draw_options = {('A', 'X'), ('B', 'Y'), ('C', 'Z')}
     shape_score = 0
     match_score = 0
     moves_set, own_moves_list = read_to_list(file)
-    '''for el in own_moves_list:
-        shape_score = shape_score + shape_score_dict[el]'''
-    print(moves_set)
-    moves_set = [('B', 'Z'), ('A', 'X'), ('B', 'Z'), ('A', 'A')]
-    print(moves_set)
+    for el in own_moves_list:
+        shape_score = shape_score + shape_score_dict[el]
     for el in moves_set:
-        print(f"el is {el}")
-        print(el[0])
-        print(el[1])
         if el in win_options:
             match_score = match_score + 6
-        elif el[0] == el[1]:
-            print(f"I got into the elif with {el}")
+        elif el in draw_options:
             match_score = match_score + 3
     total_score = shape_score + match_score
+    print(dict(moves_set))
     return total_score
+
+def get_startegic_moves(file):
+    #strategy_dict = {'X': "lose", 'Y': "draw", 'Z': "win"}
+    win_options = {'C': 'A', 'A': 'B', 'B': 'C'}
+    lose_options = {'A': 'B', 'B': 'C', 'C': 'A'}
+    moves_set, own_moves_list = read_to_list(file)
+    print(moves_set)
+    for el in moves_set:
+        print(f"here el is {el}")
+        opponent_move = el[0]
+        print(f"here opponent_move is {opponent_move}")
+        print(f"here el[1] is {el[1]}")
+        if el[1] == 'X':
+            print("first if")
+            #el[1] = lose_options[opponent_move]
+        elif el[1] == 'Z':
+            print("second if")
+            #el[1] = win_options[opponent_move]
+        else:
+            print("third if")
+            #el[1] = el[0]
+    return moves_set
 
 
 if __name__ == '__main__':
     file = "input.txt"
     read_to_list(file)
     total_score = final_result(file)
+
     #part 1
     print("PART 1\n")
     print(f"Total score would be {total_score}")
 
-    ''''#part 2
+    #part 2
     print("PART 2\n")
-
-    print(f"")'''
+    get_startegic_moves(file)
 
